18f16863c47be515510389a091c78dbcbb4ae5e3
abdukasim <abdukasimr@gmail.com>
2022-12-14 15:15:06 +0300

    feat: added searchbar and list components to attendance screen

--- src/screens/attendance/index.tsx
+++ src/screens/attendance/index.tsx
@@ -1,16 +1,42 @@
 import { View } from "react-native";
-import React from "react";
-import { Card } from "../../components/card";
-import { Text } from "../../components/text";
-import { PresentIcon } from "../../../assets/svg/icons";
-import { theme } from "../../styles/theme";
-import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
+import React, { useEffect, useState } from "react";
 import Stats from "../../components/stats";
+import { SearchBar } from "../../components/searchBar";
+import { List } from "../../components/list";
+import url from "../../helpers/url";
+import useSearch from "../../hooks/useSearch";
 
 export default function AttendanceScreen() {
+  // const [searchPhrase, setSearchPhrase] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [attendanceList, setAttendanceList] = useState([]);
+
+  const [search, filteredDataSource, searchFilterFunction] =
+    useSearch(attendanceList);
+
+  const fetchAttendanceList = async () => {
+    try {
+      const res = await url.get("/api/attendance/client");
+      setAttendanceList(res.data.list);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAttendanceList();
+  }, []);
+
   return (
     <View style={{ backgroundColor: "white", height: "100%" }}>
       <Stats />
+      <SearchBar
+        clicked={clicked}
+        setClicked={setClicked}
+        searchPhrase={search}
+        setSearchPhrase={searchFilterFunction}
+      />
+      <List data={filteredDataSource} />
     </View>
   );
 }
