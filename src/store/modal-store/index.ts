1c1bfcabe258fc4ac1ff23fb1f2d28bbb276b46f
abdukasim <abdukasimr@gmail.com>
2022-12-25 16:49:28 +0300

    feat: added visited and beneficiaries modal state

--- src/store/modal-store/index.ts
+++ src/store/modal-store/index.ts
@@ -16,6 +16,20 @@ export interface ModalStore {
     hide: () => void;
     toggle: () => void;
   };
+  visited: {
+    isShow: boolean;
+    visitedData: any;
+    show: (data: object) => void;
+    hide: () => void;
+    toggle: () => void;
+  };
+  beneficiaries: {
+    isShow: boolean;
+    beneficiariesData: any;
+    show: (data: object) => void;
+    hide: () => void;
+    toggle: () => void;
+  };
 }
 
 export const useModalStore = create<ModalStore>((set, get) => ({
@@ -66,7 +80,59 @@ export const useModalStore = create<ModalStore>((set, get) => ({
     toggle: () => {
       set(
         produce<ModalStore>((state) => {
-          state.pending.isShow = !state.attendance.isShow;
+          state.pending.isShow = !state.pending.isShow;
+        })
+      );
+    },
+  },
+  visited: {
+    isShow: false,
+    visitedData: {},
+    show: (data) => {
+      set(
+        produce<ModalStore>((state) => {
+          state.visited.isShow = true;
+          state.visited.visitedData = data;
+        })
+      );
+    },
+    hide: () => {
+      set(
+        produce<ModalStore>((state) => {
+          state.visited.isShow = false;
+        })
+      );
+    },
+    toggle: () => {
+      set(
+        produce<ModalStore>((state) => {
+          state.visited.isShow = !state.visited.isShow;
+        })
+      );
+    },
+  },
+  beneficiaries: {
+    isShow: false,
+    beneficiariesData: {},
+    show: (data) => {
+      set(
+        produce<ModalStore>((state) => {
+          state.beneficiaries.isShow = true;
+          state.beneficiaries.beneficiariesData = data;
+        })
+      );
+    },
+    hide: () => {
+      set(
+        produce<ModalStore>((state) => {
+          state.beneficiaries.isShow = false;
+        })
+      );
+    },
+    toggle: () => {
+      set(
+        produce<ModalStore>((state) => {
+          state.beneficiaries.isShow = !state.beneficiaries.isShow;
         })
       );
     },
