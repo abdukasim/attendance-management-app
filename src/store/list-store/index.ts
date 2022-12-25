af699037687c5c10192cfa7e1ec706f3c54d898a
abdukasim <abdukasimr@gmail.com>
2022-12-25 16:50:00 +0300

    feat: made list data state to global states with zustand

---
+++ src/store/list-store/index.ts
@@ -0,0 +1,80 @@
+import produce from "immer";
+import create from "zustand";
+
+import {
+  ATTENDANCE_LIST_ENDPOINT,
+  PENDING_LIST_ENDPOINT,
+  VISITED_LIST_ENDPOINT,
+} from "@env";
+
+export interface ListStore {
+  attendance: {
+    listData: [];
+    setListData: (data: []) => void;
+    endpoint: string;
+  };
+  pending: {
+    listData: [];
+    setListData: (data: []) => void;
+    endpoint: string;
+  };
+  visited: {
+    listData: [];
+    setListData: (data: []) => void;
+    endpoint: string;
+  };
+  beneficiaries: {
+    listData: [];
+    setListData: (data: []) => void;
+    endpoint: string;
+    param: { type: string };
+  };
+}
+
+export const useListStore = create<ListStore>((set) => ({
+  attendance: {
+    listData: [],
+    setListData: (data) => {
+      set(
+        produce<ListStore>((state) => {
+          state.attendance.listData = data;
+        })
+      );
+    },
+    endpoint: ATTENDANCE_LIST_ENDPOINT,
+  },
+  pending: {
+    listData: [],
+    setListData: (data) => {
+      set(
+        produce<ListStore>((state) => {
+          state.pending.listData = data;
+        })
+      );
+    },
+    endpoint: PENDING_LIST_ENDPOINT,
+  },
+  visited: {
+    listData: [],
+    setListData: (data) => {
+      set(
+        produce<ListStore>((state) => {
+          state.visited.listData = data;
+        })
+      );
+    },
+    endpoint: VISITED_LIST_ENDPOINT,
+  },
+  beneficiaries: {
+    listData: [],
+    setListData: (data) => {
+      set(
+        produce<ListStore>((state) => {
+          state.beneficiaries.listData = data;
+        })
+      );
+    },
+    endpoint: ATTENDANCE_LIST_ENDPOINT,
+    param: { type: "full" },
+  },
+}));
