2b153e1ef06536f678364cd4205189f3e8662e58
abdukasim <abdukasimr@gmail.com>
2022-12-28 10:02:55 +0300

    fix: removed fetchAttendanceList method since its not needed

--- src/services/attendance/index.ts
+++ src/services/attendance/index.ts
@@ -1,16 +1,3 @@
-import url from "../../helpers/url";
-
-export const fetchAttendanceList = async (
-  setAttendanceList: React.Dispatch<React.SetStateAction<never[]>>
-) => {
-  try {
-    const res = await url.get("/api/attendance/client");
-    setAttendanceList(res.data.list);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 export const presentAttendee = async () => {};
 
 export const permessionAttendee = async () => {};
