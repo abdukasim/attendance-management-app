4ab44417a6e5f6f3c3583e9592c21c65dd58e684
abdukasim <abdukasimr@gmail.com>
2022-12-29 14:24:27 +0300

    feat: order list

---
+++ src/screens/attendance/order-list/index.tsx
@@ -0,0 +1,38 @@
+import React, { useCallback, useEffect } from "react";
+
+//components
+import { SafeAreaView } from "react-native";
+import { List } from "../../../components/list";
+import { OrderModal } from "../../../components/modals/order-modal";
+
+//hooks
+import { useModalStore } from "../../../store/modal-store";
+import { useFocusEffect } from "@react-navigation/native";
+import { useListStore } from "../../../store/list-store";
+
+//styles
+import { styles } from "./styles";
+
+//services
+import { fetchList } from "../../../services/list";
+
+export default function OrderScreen() {
+  const modalStore = useModalStore((state) => state.order);
+  const listStore = useListStore((state) => state.order);
+
+  useEffect(() => {
+    fetchList(listStore.setListData, listStore.endpoint);
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchList(listStore.setListData, listStore.endpoint);
+    }, [])
+  );
+  return (
+    <SafeAreaView style={styles.container}>
+      <List data={listStore.listData} parent="order" />
+      {modalStore.isShow && <OrderModal />}
+    </SafeAreaView>
+  );
+}
