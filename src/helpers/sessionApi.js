0c6e8cff845b4f4d89b785c630b6d72d8018e742
abdukasim <abdukasimr@gmail.com>
2022-12-30 14:48:54 +0300

    feat: setup axios instance for both endpoint urls and added headers

---
+++ src/helpers/sessionApi.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { SESSION_API_URL } from "@env";
+
+export default axios.create({
+  baseURL: SESSION_API_URL,
+  headers: { "content-type": "application/x-www-form-urlencoded" },
+});
