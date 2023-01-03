fc69e7a4d93ff90284307a706b4a165900b2da27
abdukasim <abdukasimr@gmail.com>
2023-01-03 14:20:05 +0300

    feat: list services

--- src/services/list-service.ts
+++ src/services/list-service.ts
@@ -1,14 +1,27 @@
 import url from "../helpers/attendanceApi";
 
-export const fetchList = async (
-  setList: any,
-  endpoint: string,
-  param?: object
-) => {
-  try {
-    const res = await url.get(endpoint);
-    setList(res.data.list);
-  } catch (error) {
-    console.error(error);
+export default class list {
+  /**
+   * ### fetch list data
+   * @param setList list data setter
+   * @param endpoint endpoint to fetch from
+   */
+  static async fetchList(setList: (data: []) => void, endpoint: string) {
+    try {
+      const res = await url.get(endpoint);
+      setList(res.data.list);
+    } catch (error) {
+      console.error(error);
+    }
   }
-};
+
+  static deleteFromList(listData: any, id: number) {
+    url
+      .delete(`${listData.endpoint}/${id}`)
+      .then((res) => {
+        console.log(res.status);
+        this.fetchList(listData.setListData, listData.endpoint);
+      })
+      .catch((err) => console.error(err));
+  }
+}
