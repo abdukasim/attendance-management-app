19d2aa77f70c29ec470534a8b3228525b9eaca13
abdukasim <abdukasimr@gmail.com>
2023-01-09 17:13:34 +0300

    feat: async storage services

---
+++ src/services/storage-services.ts
@@ -0,0 +1,36 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { LoginResponse } from "../models/session-models";
+
+export default class storage {
+  static async storeData(key: string, value: LoginResponse) {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem(key, jsonValue);
+    } catch (e) {
+      // saving error
+      console.error(e);
+    }
+  }
+
+  static async getData(key: string) {
+    try {
+      const jsonValue = await AsyncStorage.getItem("@storage_Key");
+      console.log("====================================");
+      console.log({ jsonValue });
+      console.log("====================================");
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      // error reading value
+      console.error(e);
+    }
+  }
+
+  static async removeValue(key: string) {
+    try {
+      await AsyncStorage.removeItem(key);
+    } catch (e) {
+      // remove error
+      console.error(e);
+    }
+  }
+}
