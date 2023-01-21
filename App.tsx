2e8fb58a0a4dc7819c6072968ff3e23c44938797
abdukasim <abdukasimr@gmail.com>
2023-01-21 16:41:10 +0300

    feat: connect with socket on app start

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
+import { SOCKET_API_URL } from "@env";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { io } from "socket.io-client";
 
 import useCachedResources from "./src/hooks/useCachedResources";
 import { MainNavigation } from "./src/navigation/main";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
+
+  const socket = io(SOCKET_API_URL);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(socket.connected); // true
+    });
+    return () => {
+      socket && socket.disconnect();
+    };
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
