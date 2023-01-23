3e9b2041bffc0684075297b2fc6cb94f7aed30b2
abdukasim <abdukasimr@gmail.com>
2023-01-23 15:12:25 +0300

    feat: added react native root toast package

--- App.tsx
+++ App.tsx
@@ -1,38 +1,29 @@
-import { SOCKET_API_URL } from "@env";
+//libs
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { io } from "socket.io-client";
-
-import useCachedResources from "./src/hooks/useCachedResources";
 import { MainNavigation } from "./src/navigation/main";
+import { RootSiblingParent } from "react-native-root-siblings";
+
+//hooks
+import useCachedResources from "./src/hooks/useCachedResources";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-
-  const socket = io(SOCKET_API_URL);
-
-  useEffect(() => {
-    socket.on("connect", () => {
-      console.log(socket.connected); // true
-    });
-    return () => {
-      socket && socket.disconnect();
-    };
-  }, []);
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <MainNavigation />
-        <StatusBar
-          backgroundColor={"transparent"}
-          translucent
-          style="inverted"
-        />
-      </SafeAreaProvider>
+      <RootSiblingParent>
+        <SafeAreaProvider>
+          <MainNavigation />
+          <StatusBar
+            backgroundColor={"transparent"}
+            translucent
+            style="inverted"
+          />
+        </SafeAreaProvider>
+      </RootSiblingParent>
     );
   }
 }
