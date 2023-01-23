000d9759c7ca9c97fd75fa26bb57b33119d0f839
abdukasim <abdukasimr@gmail.com>
2023-01-23 15:13:36 +0300

    feat: added a 10 second timeout

--- src/helpers/attendanceApi.js
+++ src/helpers/attendanceApi.js
@@ -3,4 +3,5 @@ import { API_URL } from "@env";
 
 export default axios.create({
   baseURL: API_URL,
+  timeout: 10000,
 });
--- src/helpers/sessionApi.js
+++ src/helpers/sessionApi.js
@@ -4,4 +4,5 @@ import { SESSION_API_URL } from "@env";
 export default axios.create({
   baseURL: SESSION_API_URL,
   headers: { "content-type": "application/x-www-form-urlencoded" },
+  timeout: 10000,
 });
